416bff37b1ec6def17bd98439123d80d7d0f6855
losmalditosdeagronomia <losmalditosdeagronomia@gmail.com>
2026-09-18 11:11:34 -0500

    Update hola.py

--- hola.py
+++ hola.py
@@ -326,5 +326,12 @@ def descargar():
     )
 
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(
+        host="0.0.0.0",
+        port=port,
+        debug=False
+    )
